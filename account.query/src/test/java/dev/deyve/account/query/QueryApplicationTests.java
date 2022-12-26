57b2b87b61b3d89ecf70bc372a41d6a2523d5b64
Deyve Machado <deyvedvm@hotmail.com>
2022-12-26 17:04:22 -0300

    Section 2: Setup & Structure

---
+++ account.query/src/test/java/dev/deyve/account/query/QueryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.deyve.account.query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
